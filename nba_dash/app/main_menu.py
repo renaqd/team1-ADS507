e100faf4b1b22b81dc6ccc99555555824a0d69c2
nolan-peters <nolanpeters@sandiego.edu>
2025-02-23 18:51:05 -0800

    added new dashboard for player stats

--- nba_dash/app/main_menu.py
+++ nba_dash/app/main_menu.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import team_dashboard
 import player_dashboard
+import dashboard
 
 def main():
-    st.title("NBA Hustle Stats Dashboard")
+    #st.title("NBA Hustle Stats Dashboard")
     
-    menu = ["Team Comparison", "Player Comparison"]
+    menu = ["Team Comparison", "Player Comparison", "Player 2"]
     choice = st.sidebar.selectbox("Select Dashboard", menu)
     
     if choice == "Team Comparison":
         team_dashboard.run()
     elif choice == "Player Comparison":
         player_dashboard.run()
+    elif choice == "Player 2":
+        dashboard.run()
+
 
 if __name__ == "__main__":
     main()
