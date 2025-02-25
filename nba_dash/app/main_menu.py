d6e88a9c004926f4abd260c9df20e5616877fe40
nolan-peters <nolanpeters@sandiego.edu>
2025-02-24 21:05:49 -0800

    bug fix

--- nba_dash/app/main_menu.py
+++ nba_dash/app/main_menu.py
@@ -4,13 +4,12 @@ import player_dashboard
 import dashboard
 import team_v_team
 
-st.set_page_config(
+def main():
+
+    st.set_page_config(
     page_title="NBA Hustle Dashboard",
     page_icon=":basketball:"
 )
-    
-
-def main():
 
     menu = ["Team Overview", "Player Overview", "Player v Player", "Team v Team"]
     choice = st.sidebar.selectbox("Select Dashboard", menu)
